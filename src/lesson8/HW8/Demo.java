1469b514679aaccbeb08d4ddb18f2bd37c621adc
mashaJM <masha.chip@gagadget.com>
2018-04-19 14:29:53 +0300

    all

--- src/lesson8/HW8/Demo.java
+++ src/lesson8/HW8/Demo.java
@@ -7,7 +7,7 @@ public class Demo {
         Adder adder = new Adder();
         int[] array = {5, 120, 1, 8, 0};
         adder.check(array);
-        adder.add(5,2); 
+        adder.add(5,2);
         System.out.println(array[array.length - 1]);
         System.out.println(adder.add(array[1], 2));
 
